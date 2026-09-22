677bf734f74dc1c619a392da2e9e207a76a70285
Kaisar Kadyrov <usernewuser0614@gmail.com>
2026-09-22 21:34:04 +0500

    Task1_1.1.

--- lab2/lab2_advanced_ddl.sql
+++ lab2/lab2_advanced_ddl.sql
@@ -7,3 +7,16 @@ CREATE DATABASE university_main
     OWNER CURRENT_USER
     TEMPLATE template0
     ENCODING 'UTF8';
+
+2.
+CREATE DATABASE university_archive
+    TEMPLATE template0
+    CONNECTION LIMIT 50;
+
+
+3.
+CREATE DATABASE university_test
+    CONNECTION LIMIT 10;
+ 
+ALTER DATABASE university_test WITH IS_TEMPLATE = true;
+
