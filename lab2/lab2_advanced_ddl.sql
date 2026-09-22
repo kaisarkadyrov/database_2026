721b97db22eaf0adfa0c601922b4b9aec22b60c0
Kaisar Kadyrov <usernewuser0614@gmail.com>
2026-09-22 21:57:26 +0500

    Task2_2.2.

--- lab2/lab2_advanced_ddl.sql
+++ lab2/lab2_advanced_ddl.sql
@@ -78,3 +78,29 @@ CREATE TABLE courses (
     is_online        BOOLEAN,
     created_at       TIMESTAMP WITHOUT TIME ZONE
 );
+
+Task 2.2.
+1.
+CREATE TABLE class_schedule (
+    schedule_id      SERIAL PRIMARY KEY,
+    course_id        INTEGER,
+    professor_id     INTEGER,
+    classroom        VARCHAR(20),
+    class_date       DATE,
+    start_time       TIME WITHOUT TIME ZONE,
+    end_time         TIME WITHOUT TIME ZONE,
+    duration         INTERVAL
+);
+
+2.
+CREATE TABLE student_records (
+    record_id              SERIAL PRIMARY KEY,
+    student_id             INTEGER,
+    course_id              INTEGER,
+    semester               VARCHAR(20),
+    year                   INTEGER,
+    grade                  CHAR(2),
+    attendance_percentage  NUMERIC(4,1),
+    submission_timestamp   TIMESTAMP WITH TIME ZONE,
+    last_updated           TIMESTAMP WITH TIME ZONE
+);
